e56bffa690e1d5249705064e8c94ed31ea90ef60
Ms Eko <jomaduagwu@gmail.com>
2023-07-21 00:07:21 -0500

    updated resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,86 +1,74 @@
-// const { AuthenticationError } = require('apollo-server-express');
-// const { Profile } = require('../models');
-// const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
-// const resolvers = {
-//   Query: {
-//     profiles: async () => {
-//       return Profile.find();
-//     },
+const resolvers = {
+  Query: {
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+        .select("-__v -password")
+        .populate("books");
 
-//     profile: async (parent, { profileId }) => {
-//       return Profile.findOne({ _id: profileId });
-//     },
-//     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-//     me: async (parent, args, context) => {
-//       if (context.user) {
-//         return Profile.findOne({ _id: context.user._id });
-//       }
-//       throw new AuthenticationError('You need to be logged in!');
-//     },
-//   },
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 
-//   Mutation: {
-//     addProfile: async (parent, { name, email, password }) => {
-//       const profile = await Profile.create({ name, email, password });
-//       const token = signToken(profile);
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-//       return { token, profile };
-//     },
-//     login: async (parent, { email, password }) => {
-//       const profile = await Profile.findOne({ email });
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-//       if (!profile) {
-//         throw new AuthenticationError('No profile with this email found!');
-//       }
+      if (!user) {
+        throw new AuthenticationError('No user with this email found!');
+      }
 
-//       const correctPw = await profile.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-//       if (!correctPw) {
-//         throw new AuthenticationError('Incorrect password!');
-//       }
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
 
-//       const token = signToken(profile);
-//       return { token, profile };
-//     },
+      const token = signToken(user);
+      return { token, user };
+    },
 
-//     // Add a third argument to the resolver to access data in our `context`
-//     addSkill: async (parent, { profileId, skill }, context) => {
-//       // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
-//       if (context.user) {
-//         return Profile.findOneAndUpdate(
-//           { _id: profileId },
-//           {
-//             $addToSet: { skills: skill },
-//           },
-//           {
-//             new: true,
-//             runValidators: true,
-//           }
-//         );
-//       }
-//       // If user attempts to execute this mutation and isn't logged in, throw an error
-//       throw new AuthenticationError('You need to be logged in!');
-//     },
-//     // Set up mutation so a logged in user can only remove their profile and no one else's
-//     removeProfile: async (parent, args, context) => {
-//       if (context.user) {
-//         return Profile.findOneAndDelete({ _id: context.user._id });
-//       }
-//       throw new AuthenticationError('You need to be logged in!');
-//     },
-//     // Make it so a logged in user can only remove a skill from their own profile
-//     removeSkill: async (parent, { skill }, context) => {
-//       if (context.user) {
-//         return Profile.findOneAndUpdate(
-//           { _id: context.user._id },
-//           { $pull: { skills: skill } },
-//           { new: true }
-//         );
-//       }
-//       throw new AuthenticationError('You need to be logged in!');
-//     },
-//   },
-// };
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { bookData}, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {$addToSet: { savedBooks: bookData }, },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    removeBook: async (parent, {bookId}, context) => {
+      if (context.user) {
+        return User.findOneAndDelete(
+            { _id: context.user._id },
+            { $pull: { savedBooks: {bookId: params.bookId } } },
+            { new: true },
+            );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
 
-// module.exports = resolvers;
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,34 +1,47 @@
 const { gql } = require('apollo-server-express');
 
-// const typeDefs = gql`
-//   type Profile {
-//     _id: ID
-//     name: String
-//     email: String
-//     password: String
-//     skills: [String]!
-//   }
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-//   type Auth {
-//     token: ID!
-//     profile: Profile
-//   }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
 
-//   type Query {
-//     profiles: [Profile]!
-//     profile(profileId: ID!): Profile
-//     # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-//     me: Profile
-//   }
+  type savedBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
 
-//   type Mutation {
-//     addProfile(name: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
+   type Auth {
+     token: ID!
+     user: User
+   }
 
-//     addSkill(profileId: ID!, skill: String!): Profile
-//     removeProfile: Profile
-//     removeSkill(skill: String!): Profile
-//   }
-// `;
+   type Query {
+     me: User
+   }
 
-// module.exports = typeDefs;
+   type Mutation {
+     login(email: String!, password: String!): Auth
+     addUser(username: String!, email: String!, password: String!): Auth
+     saveBook(bookData: savedBook!): User
+     removeBook(bookId: ID!): User
+   }
+ `;
+
+ module.exports = typeDefs;
