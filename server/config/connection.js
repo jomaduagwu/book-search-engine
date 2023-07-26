c01856712230c1eba91d942d72d823fa6f55cb2d
Ms Eko <jomaduagwu@gmail.com>
2023-07-26 12:59:02 -0500

    updated connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jomaduagwu:ekojonwa23@cluster0.g6xtvyl.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
+
+// 'mongodb://127.0.0.1:27017/googlebooks'
